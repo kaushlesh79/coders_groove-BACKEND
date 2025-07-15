69d8da8337352d04bba17eb53e45c0bdfd5ba540
kaushlesh79 <kaushleshchaurasiya196@gmail.com>
2025-07-15 18:57:29 +0530

    Remove frontend serving from backend

--- index.js
+++ index.js
@@ -10,11 +10,9 @@ const server = http.createServer(app);
 
 const io = new Server(server);
 
-app.use(express.static(path.join(__dirname, "../client/build")));
-
 app.use((req, res, next) => {
-    res.sendFile(path.join(__dirname, "../client/build/index.html"));
-})
+    res.status(404).json({ error: "API route not found" });
+});
 
 
 
